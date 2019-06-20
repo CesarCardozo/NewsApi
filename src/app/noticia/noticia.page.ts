e7c31ad8e28c63dac9a94b610fd6214ca5d96014
Cesar Nicolas Cardozo Rincón <srfeudalwow@gmail.com>
2019-06-19 19:03:38 -0500

    noticia en componentes

--- src/app/noticia/noticia.page.ts
+++ src/app/noticia/noticia.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-noticia',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoticiaPage implements OnInit {
 
+  @Input() new;
   constructor() { }
 
   ngOnInit() {
--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
+import { NoticiaPage } from '../noticia/noticia.page';
+import { NoticiaPageModule } from '../noticia/noticia.module';
 
 @NgModule({
+  entryComponents:[
+    NoticiaPage
+  ],
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    NoticiaPageModule,
     RouterModule.forChild([{ path: '', component: Tab2Page }])
   ],
   declarations: [Tab2Page]
